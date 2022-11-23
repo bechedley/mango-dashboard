e354fa1f7d96c30ddb63f7b040374f48c8faea65
wongpakho814 <wongpakho814@gmail.com>
2022-11-23 16:09:55 +0800

    updated auth.handlebars form ids, updated server.js to include cookies

--- server.js
+++ server.js
@@ -2,17 +2,22 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
 const routes = require("./controllers");
+
 const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: "Super secret secret",
-  cookie: {},
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
