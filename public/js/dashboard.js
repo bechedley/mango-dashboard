9f5632b2685cbe9ccc3e23851845b1a09b17c3e8
wongpakho814 <wongpakho814@gmail.com>
2022-11-24 16:55:38 +0800

    Implemented projectRoutes
    user can now browse the project the project details page by clicking on the project in dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,10 +1,18 @@
+const projectClickHandler = (event) => {
+  // Get the id of the project to be rendered
+  if (event.currentTarget.hasAttribute("data-id")) {
+    let id = event.currentTarget.getAttribute("data-id");
+    document.location.assign(`/project/${id}`);
+  }
+};
+
 // const newFormHandler = async (event) => {
 //     event.preventDefault();
-  
+
 //     const name = document.querySelector('#project-name').value.trim();
 //     const needed_funding = document.querySelector('#project-budget').value.trim();
 //     const summary = document.querySelector('#project-desc').value.trim();
-  
+
 //     if (name && needed_funding && description) {
 //       const response = await fetch(`/api/projects`, {
 //         method: 'POST',
@@ -13,7 +21,7 @@
 //           'Content-Type': 'application/json',
 //         },
 //       });
-  
+
 //       if (response.ok) {
 //         document.location.replace('/profile');
 //       } else {
@@ -21,15 +29,15 @@
 //       }
 //     }
 //   };
-  
+
 //   const delButtonHandler = async (event) => {
 //     if (event.target.hasAttribute('data-id')) {
 //       const id = event.target.getAttribute('data-id');
-  
+
 //       const response = await fetch(`/api/projects/${id}`, {
 //         method: 'DELETE',
 //       });
-  
+
 //       if (response.ok) {
 //         document.location.replace('/profile');
 //       } else {
@@ -37,12 +45,17 @@
 //       }
 //     }
 //   };
-  
+
 //   document
 //     .querySelector('.new-project-form')
 //     .addEventListener('submit', newFormHandler);
-  
+
 //   document
 //     .querySelector('.project-list')
 //     .addEventListener('click', delButtonHandler);
-  
+
+const projects = document.querySelectorAll(".project-div");
+
+for (let i = 0; i < projects.length; i++) {
+  projects[i].addEventListener("click", projectClickHandler);
+}
